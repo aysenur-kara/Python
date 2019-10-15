e03dd0e45a964c4b2419dcf65a9254c2fd1c3110
aysenur-kara <46405333+aysenur-kara@users.noreply.github.com>
2019-10-15 12:07:51 +0300

    Add files via upload

---
+++ python_sürüm_bilgisi/python_sürüm_bilgisi/python_sürüm_bilgisi.py
@@ -0,0 +1,7 @@
+def python_surum_bilgisi():
+    import sys
+    print("ana sürüm numarası:",sys.version_info.major)
+    print("alt sürüm numarası:",sys.version_info.minor)
+    print("mikro sürüm numarası:",sys.version_info.micro)
+
+python_surum_bilgisi()

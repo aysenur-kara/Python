802036028692e9bdaeea6c155ae5beb274ded1c9
aysenur-kara <46405333+aysenur-kara@users.noreply.github.com>
2019-10-23 10:46:58 +0300

    Add files via upload

---
+++ ParolaDogrulama/ParolaDogrulama.py
@@ -0,0 +1,24 @@
+kullanici_adi = "aysenur"
+sifre = "1234"
+
+kullanici_adi_gir=input("Kullanici adinizi giriniz: ")
+sifre_gir=input("Sifrenizi giriniz: ")
+
+if kullanici_adi==kullanici_adi_gir and sifre==sifre_gir:
+    print("Dogrulama Basarili")
+    
+
+
+elif kullanici_adi!=kullanici_adi_gir or sifre!=sifre_gir :
+            print("Kullanici Adi Hatali veya Sifre Hatalı")
+    
+
+    
+
+         
+     
+             
+            
+        
+       
+
